5e0d02c4e5e8ab20911f4fa8c697f28b542db7ec
Abhi Rajendran <24395ar@hvhs.school.nz>
2026-04-29 13:31:03 +1200

    doing functions

--- t03_maths.js
+++ t03_maths.js
@@ -12,10 +12,12 @@ var age=15
 var number1=502.5
 var number2=10
 var number3=2
-var dollar=67
+var dollar=0.67
+var number4=3
 var futureage= age + number2
 var moneyleft= dollar/number3
-console.log("Hi, My name is "+name+". As of "+year+", I'm "+age+" years old. I was born in "+ number1 * 4 +". In 10 years, I will be "+futureage+ ". I have "+dollar+" cents. I spend half of my money, now I have "+ moneyleft+" cents.");
+var addmoney= moneyleft + number4
+console.log("Hi, My name is "+name+". As of "+year+", I'm "+age+" years old. I was born in "+ number1 * 4 +". In 10 years, I will be "+futureage+ ". I have "+dollar+" cents. I spend half of my money, now I have "+ moneyleft+" cents. Then I got $3, Now I have "+addmoney+" cents.");
 
 
 
