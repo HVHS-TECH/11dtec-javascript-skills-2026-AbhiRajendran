0949fc62c2dae0f5b4bd5e712629a88479f8102b
Abhi Rajendran <24395ar@hvhs.school.nz>
2026-04-30 15:13:48 +1200

    finished debugging

--- t06_functions.js
+++ t06_functions.js
@@ -1,1 +1,40 @@
-/* Paste your code from task 5 here */
+/* 
+Introduction
+*/
+console.log("Running t01_variables.js")
+console.log("Is it running? Haha its working.")
+
+//Variables
+
+var year = "2026"
+var name = "Abhi"
+var age = 15
+var number1 = 502.5
+var number2 = 10        
+var number3 = 2
+var dollar = 0.67
+var number4 = 3
+var futureage = age + number2
+var moneyleft = dollar / number3
+var addmoney = moneyleft + number4
+console.log("Hi, My name is " + name + ". As of " + year + ", I'm " + age + " years old. I was born in " + number1 * 4 + ". In 10 years, I will be " + futureage + ". I have " + dollar + " cents. I spend half of my money, now I have " + moneyleft + " cents. Then I got $3, Now I have " + addmoney + " cents.");
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput")
+OUTPUT.innerHTML = "<h2>Hi, My name is " + name + ". As of " + year + ", I'm " + age + " years old. I was born in " + number1 * 4 + ". In 10 years, I will be " + futureage + ". I have " + dollar + " cents. I spend half of my money, now I have " + moneyleft + " cents. Then I got $3, Now I have " + addmoney + " cents.</h2>"
+
+
+
+/*
+Main Code
+*/
+
+
+
+
+
+
+
+
+/*
+Functions
+*/
