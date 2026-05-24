6c0f9c310d8ad1cdf684c12343e9b7a0a2da330e
Abhi Rajendran <24395ar@hvhs.school.nz>
2026-05-25 09:53:56 +1200

    working

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -31,13 +31,12 @@ writeLine()
 
 
 
-
 /*
 Functions
 */
 
 function writeLine() {
-    OUTPUT.innerHTML += "<h2> Welcome to the Shop</h2>";
+    OUTPUT.innerHTML = "<h2> Welcome to the Shop</h2>";
 }
 
 
@@ -45,16 +44,15 @@ function getFormInput() {
     const NAME_FIELD = document.getElementById("nameField");
     const AGE_FIELD = document.getElementById("ageField");
     const MONEY_FIELD = document.getElementById("moneyField");
-    let userName = NAME_FIELD.value;
-    let userAge = AGE_FIELD.value;
-    let userMoney = Boolean(MONEY_FIELD.value);
-    OUTPUT.innerHTML = "<h2>Welcome to the Shop, " + userName + ". You are " + userAge + " years old. You have " + userMoney + " dollars.</h2>";
+    let userName = String(NAME_FIELD.value);
+    let userAge = Number(AGE_FIELD.value);
+    let userMoney = Number(MONEY_FIELD.value);
 
     if (userMoney >= 4) {
-        OUTPUT.innerHTML = "<h2>You can buy a $4 chocolate Bar. </h2>";
+        OUTPUT.innerHTML = "<h2>Welcome to the Shop, " + userName + ". You are " + userAge + " years old. You have " + userMoney + " dollars.You can buy a $4 chocolate Bar. </h2>";
     }
-    else (userMoney < 4); {
-        OUTPUT.innerHTML = "<h2>You can't even afford a chocolate Bar. Get Out!!</h2>";
+    else{
+        OUTPUT.innerHTML = "<h2>Welcome to the Shop, " + userName + ". You are " + userAge + " years old. You have " + userMoney + " dollars.You can't even afford a chocolate Bar. Get Out!!</h2>";
     }
 }
 
